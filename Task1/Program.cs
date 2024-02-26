7074ad5b74993e0291f8f154c0147e00710daa3e
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-26 23:31:47 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+
+void Main()
+{
+    int M = ReadInt("Введите нижнюю границу: ");
+    int N = ReadInt("Введите верхнюю границу: ");
+    PrintNumbers(M, N);
+}
+
+void PrintNumbers(int M, int N)
+{
+    if(M == N + 1) return;
+    
+    System.Console.Write(M + " ");
+    PrintNumbers(M + 1, N);
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
